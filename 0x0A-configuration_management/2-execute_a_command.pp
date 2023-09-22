7cdf9fadd192ffdaee31a9af1bcb138af2a14ea0
Easykeljohn <easykelchimdikejohn@gmail.com>
2023-09-22 09:20:52 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-# a manifest that kills a process named killmenow.
+# create a process named killmenow 
 
-exec { 'pkill killmenow':
-    path     => '/bin/',
-    command  => 'pkill killmenow',
-}
+exec { 'pkill killmenow' :
+    path    => '/bin/',
+    command => 'pkill killmenow',
+    }
