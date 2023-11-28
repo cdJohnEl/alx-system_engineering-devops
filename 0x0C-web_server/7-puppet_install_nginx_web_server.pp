f902385d7e8eaf67ee42aee5297c142da7111c0e
Easykeljohn <easykelchimdikejohn@gmail.com>
2023-11-28 11:09:27 +0100

    Correction to task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,20 @@
-# Install Nginx web server with Puppet
+# Install Nginx web server (w/ Puppet)
 package { 'nginx':
-    ensure => installed,
+  ensure => installed,
 }
 
-file_line { 'goodGod':
-    ensure  => 'present',
-    path    => '/etc/nginx/sites-available/default',
-    after   => 'listen 80 default-server;',
-    line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=iv1_B29wQy0;',
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 file { '/var/www/html/index.html':
-    content => 'Hello World!',
+  content => 'Hello World!',
 }
 
 service { 'nginx':
-    ensure  => running,
-    require => Package['nginx'],
+  ensure  => running,
+  require => Package['nginx'],
 }
